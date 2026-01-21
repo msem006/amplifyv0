f451c0a7f2030fb561940473f6fa3be2173716b8
Amplify Dev <dev@amplify.gg>
2026-01-21 13:07:10 +1100

    Robust: Add trustHost: true for Vercel compatibility

--- src/auth.ts
+++ src/auth.ts
@@ -59,5 +59,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     pages: {
         // Force the sign-in flow to use our custom landing page buttons
         signIn: '/'
-    }
+    },
+    trustHost: true
 })
